6112f8b3d0a47fc30616fe6c2f04cd2ff793ebdb
froilanbuendia <froilangbuendia@gmail.com>
2023-07-26 14:37:22 -0700

    changed package.json and eslint.json files

--- backend/app.js
+++ backend/app.js
@@ -49,8 +49,9 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
-
-module.exports = app;
