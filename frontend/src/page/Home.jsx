3142081e83b5ff8c102b85400cc9be673f2b7889
froilanbuendia <froilangbuendia@gmail.com>
2023-08-21 18:37:23 -0700

    created play area template

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import hearts from '../assets/hearts.svg';
+import './home.css';
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div className="play-area">
+        <div className="upper-play-area">
+          <div className="time-hits">
+            <p className="time">Time:</p>
+            <p className="hits">Hits:</p>
+          </div>
+          <div className="hearts">
+            <img src={hearts} alt="lives" />
+          </div>
+        </div>
+        <div className="play-box">play Area</div>
+      </div>
     </div>
   );
 }
